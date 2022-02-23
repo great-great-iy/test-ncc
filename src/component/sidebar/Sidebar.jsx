6203b9c32e488a2166add425d934bcdc6082d01f
Nam Nguyen <87434122+great-great-iy@users.noreply.github.com>
2022-02-24 00:01:34 +0700

    update

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ export function Sidebar(){
     return(<>
         <section className='ncc__sidebar'>
             <ul className='ncc__sidebar--navbar'>
-                <li><a href=''>Home</a></li>
+                <li><a className='active' href=''>Home</a></li>
                 <li><a href=''>Services</a></li>
                 <li><a href=''>News</a></li>
                 <li><a href=''>Blog</a></li>
