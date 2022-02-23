ff0184930992ee8b59a8c3d043cb31bf0e4d9237
Nam Nguyen <87434122+great-great-iy@users.noreply.github.com>
2022-02-23 20:48:25 +0700

    test

---
+++ src/component/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Sidebar(){
+    return(<>
+        <section>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>News</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    </>);
+}
